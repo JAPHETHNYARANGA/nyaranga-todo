6ce31e45fcde48b8601c5f4cef006812436534ca
JAPHETHNYARANGA <nyaranga4@gmail.com>
2021-09-17 13:10:28 +0300

    first

---
+++ src/main/java/models/Task.java
@@ -0,0 +1,75 @@
+package models;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class Task {
+
+    private String description;
+    private boolean completed;
+    private LocalDateTime createdAt;
+    private int id;
+    private int categoryId;
+
+    public Task(String description, int categoryId){
+        this.description = description;
+        this.completed = false;
+        this.createdAt = LocalDateTime.now();
+        this.categoryId = categoryId;
+    }
+
+
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Task)) return false;
+        Task task = (Task) o;
+        return completed == task.completed &&
+                id == task.id &&
+                Objects.equals(description, task.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(description, completed, id);
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean getCompleted(){
+        return this.completed;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+}
